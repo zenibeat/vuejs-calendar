d5d5012a66a36a0fa2e3a044cef87558ba8c92d0
Beat Zenerino <beat@bitvana.com>
2017-09-28 15:04:46 -0500

    finished

--- server.js
+++ server.js
@@ -5,13 +5,46 @@ const app = express();
 const path = require('path');
 const fs = require('fs');
 const http = require('http');
+const moment = require('moment-timezone');
+moment.tz.setDefault('UTC');
+const serialize = require('serialize-javascript');
 
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
+let events = [
+	{description: "Random Event 1", date: moment('2017-09-06', 'YYYY-MM-DD')},
+	{description: "Random Event 2", date: moment('2017-09-15', 'YYYY-MM-DD')},
+	{description: "Random Event 3", date: moment('2017-09-21', 'YYYY-MM-DD')}
+];
+
+let renderer;
+
+if (process.env.NODE_ENV === 'production') {
+	let bundle = fs.readFileSync('./dist/node.bundle.js', 'utf8');
+	renderer = require('vue-server-renderer').createBundleRenderer(bundle);
+	app.use('/dist', express.static(path.join(__dirname, 'dist')));
+}
+
 app.get('/', (req, res) => {
   let template = fs.readFileSync(path.resolve('./index.html'), 'utf-8');
-  res.send(template);
+  let contentMarker = '<!--APP-->'
+	if (renderer) {
+		renderer.renderToString({events}, (err, html) => {
+			if (err) {
+				console.log(err);
+			} else {
+				res.send(template.replace(contentMarker, `<script>var __INTITIAL_STATE__ = ${serialize(events)}</script>\n${html}`));
+			}
+		});
+	} else {
+  	res.send('<p>Awaiting compilation...</p><script src="/reload/reload.js"></script>');
+	}
+});
 
+app.use(require('body-parser').json());
+app.post('/add_event', (req, res) => {
+  events.push(req.body);
+  res.sendStatus(200);
 });
 
 const server = http.createServer(app);
@@ -20,6 +53,13 @@ if (process.env.NODE_ENV === 'development') {
   const reload = require('reload');
   const reloadServer = reload(server, app);
   require('./webpack-dev-middleware').init(app);
+  require('./webpack-server-compiler').init(function(bundle) {
+  	let needsReload = (renderer === undefined);
+  	renderer = require('vue-server-renderer').createBundleRenderer(bundle);
+  	if (needsReload){
+  		reloadServer.reload();
+	  }
+  });
 }
 
 server.listen(process.env.PORT, function () {
